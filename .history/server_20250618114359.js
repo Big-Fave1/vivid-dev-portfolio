2e80441d71f3a76d52ed24149fa1c9328bdf2e00
Favour chimdiogo <osonduf33@gmail.com>
2025-06-18 11:51:44 +0100

    first commit

---
+++ .history/server_20250618114359.js
@@ -0,0 +1,44 @@
+
+
+require('dotenv').config();
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post('/contact', (req, res) => {
+  const { name, email, subject, message } = req.body;
+  console.log('Received message:', { name, email, subject, message });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL_USER,
+    subject: subject ? subject : `New Contact Message from ${name}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ success: false, message: 'Failed to send email.' });
+    }
+    res.status(200).json({ success: true, message: 'Message received and email sent!' });
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
